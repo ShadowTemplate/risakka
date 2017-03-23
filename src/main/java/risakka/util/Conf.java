95f8c2b21e7706346558e18b21b6dc4d093b71a9
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2017-03-23 15:10:00 +0100

    * Removed hard-coded application.conf path

--- src/main/java/risakka/util/Conf.java
+++ src/main/java/risakka/util/Conf.java
@@ -1,29 +1,39 @@
 package risakka.util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.lang.reflect.Field;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class Conf {
 
+    private static final String PROPERTIES_FILE = "application.conf";
+
     public static final String CLUSTER_NAME;
     public static final int SERVER_NUMBER;
     public static final int HEARTBEAT_FREQUENCY;  // TODO make sure it is consistent with the avg broadcasting time
+
     public static final int MAX_CLIENT_SESSIONS;
-    
     public static final String[] NODES_IPS;
     public static final String[] NODES_PORTS;
+
     public static final String LOG_FOLDER;
     public static final String BASE_DIR;
 
     static {
-        String propertiesFile = "src/main/resources/application.conf";//System.getProperty("risakka");
-        System.out.println("Trying to load properties from file: " + propertiesFile);
+        System.out.println("Trying to load properties from file: " + PROPERTIES_FILE);
+
         Properties prop = new Properties();
         try {
+            URL resource = Conf.class.getClassLoader().getResource(PROPERTIES_FILE);
+            File propertiesFile = Paths.get(resource.toURI()).toFile();
             prop.load(new FileInputStream(propertiesFile));
+            System.out.println("Configuration successfully loaded from file: " + PROPERTIES_FILE);
         } catch (Exception ex) {
             System.err.println("Error while loading properties: " + ex.getMessage());
+            System.err.println("Default configuration will be used instead.");
         }
 
         CLUSTER_NAME = prop.getProperty("CLUSTER_NAME", "raft-cluster");
